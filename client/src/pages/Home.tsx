675c036b0111741d8208c12e9433dd69369f4fb5
SaketSinghRajput <thesaketsingh2605@gmail.com>
2025-10-24 22:40:20 +0530

    remove pricing section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -54,7 +54,7 @@ export default function Home() {
       <main>
         <Hero />
         <Services />
-        <Pricing />
+       
         <Portfolio />
         <WhyChooseUs />
         <Stats />
